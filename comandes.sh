f248183d00e9fef4bbbe0a66e70d28afeb04908c
Sergi Giraldo <sergigiraldocorbo06@gmail.com>
2026-03-23 13:15:35 +0100

    Nous fitxers creats i modificats

---
+++ comandes.sh
@@ -0,0 +1,12 @@
+mkdir documents
+cd documents
+mkdir Complexos
+mkdir Inequacions
+mkdir imatges
+cd imatges
+touch triangles_tikz.png
+echo "La imatge ha estat descarregada de texample.net" >> info_imatge.txt
+
+
+
+
