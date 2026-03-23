5868ca42510705a6ccd34e42ef2d332aa1983e49
Sergi Giraldo <sergigiraldocorbo06@gmail.com>
2026-03-23 13:33:40 +0100

    Fitxers modificats

--- comandes.sh
+++ comandes.sh
@@ -2,6 +2,9 @@ mkdir documents
 cd documents
 mkdir Complexos
 mkdir Inequacions
+cd ../
+cp exercici1.tex /documents/Inequacions
+cp exercici2.tex /documents/Complexos
 mkdir imatges
 cd imatges
 touch triangles_tikz.png
